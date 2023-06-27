02ccd8d4f0914de942cdbaf8d3219e360c044966
Alice <xinranjiang2015@yahoo.com>
2023-06-27 15:47:14 -0400

    Initial Commit

---
+++ bluetoothConnect/bluetoothConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  bluetoothConnectApp.swift
+//  bluetoothConnect
+//
+//  Created by apple on 2023/6/27.
+//
+
+import SwiftUI
+
+@main
+struct bluetoothConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
